8b55c15a7487dc57351945f937579722dfa308e6
WeiTingChen1217 <t104368022@ntut.org.tw>
2016-07-22 13:34:24 +0800

    test
    test

---
+++ histogramEqualization_OpenCV/histogramEqualization_OpenCV/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  histogramEqualization_OpenCV
+//
+//  Created by WeiTingChen on 2016/7/22.
+//  Copyright © 2016年 WeiTingChen. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
